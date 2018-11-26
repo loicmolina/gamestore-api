b609a147811cf9528a8d0cb8f8b8eda596433a18
loicmolina <loicmolina@outlook.com>
2018-11-26 17:22:42 +0100

    modif mauvais package

--- src/main/java/fr/mim/gamestoreAPI/Application.java
+++ src/main/java/fr/mim/gamestoreAPI/Application.java
@@ -1,4 +1,4 @@
-package fr.mim.gamestoreAPI.serveur;
+package fr.mim.gamestoreAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
